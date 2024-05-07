15d4f878e693b05084c3c2a27c3ec48c9419c952
Laurine Maina <mainalaureen58@gmail.com>
2024-05-07 11:09:04 +0300

    initial form commit

---
+++ src/Pages/Form.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./form.css"
+
+function Form(){
+return(
+<div>
+    <form>
+        <input>Id:</input>
+        <input>Category:</input>
+        <input>Image:</input>
+        <input>Description: </input>
+        <input>Price:</input>
+    </form>
+    
+</div>)
+
+}
+export default Form
