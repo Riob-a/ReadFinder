071318abece72d759f02311b8c7490695907c0fe
Derrick <riobad74@gmail.com>
2024-05-07 09:27:19 +0300

    updated dbjson/nav-footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,22 @@
+//commit your own branch
+
+import React from "react";
+
+function Footer(){
+    return(
+        <div className="">
+
+         <footer class="footer p-5 bg-warning"> 
+            <div class="container"> 
+              <div class="row"> 
+                <div class="col-md-3"> 
+                    <h2>ReadFinder</h2> 
+                </div> 
+              </div> 
+            </div> 
+         </footer>
+
+        </div>
+    );
+}
+export default Footer;
