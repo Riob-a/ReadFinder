e173377ab8f03704ca1303f37bb4d8e26170d942
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-07 22:07:43 +0300

    link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
-//commit your own branch
 //https://project2-db.onrender.com/books
 import React from "react";
+import {Link} from "react-router-dom"
 
 function Navbar(){
     return(
@@ -8,17 +8,17 @@ function Navbar(){
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <a className="navbar-brand p-3" href="#"><b>ReadFinder</b></a>
+      <Link className="navbar-brand p-3" to="/"><b>ReadFinder</b></Link>
       <div className="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01"> 
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
           <li className="nav-item active p-3">
-            <a className="nav-link" href="#">Home </a>
+            <Link className="nav-link" to="/">Home </Link>
           </li>
           <li className="nav-item p-3">
-            <a className="nav-link" href="#">Add Book</a>
+            <Link className="nav-link" to="/form">Add Book</Link>
           </li>
           <li className="nav-item p-3">
-            <a className="nav-link" href="#">Shelf</a>
+            <Link className="nav-link" to="/MyShelves">Shelf</Link>
           </li>
         </ul>
        
