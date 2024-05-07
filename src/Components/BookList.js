0b19a93fe4ee20f72afe8ac13a4888b5169d209f
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-07 09:08:51 +0300

    btn

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -3,38 +3,48 @@ import React from 'react'
 function BookList() {
   return (
   <div className="column-grid">
-      <div className="book-div">
-              <div className="image-container">
-                  <img className='image' alt="" src='https://github.com/MohamedAhmeDdev/eBook-Next-/blob/master/server/Images/1671463217357.jpg?raw=true'  />
-              </div>
-              <div className="item-container">
-                  <p>name</p>
-                  <small>Ipsum duis nulla non reprehenderit labore incididunt.Ex amet sit minim et. </small>
-                  <p>ksh 1000</p>
-              </div>
-      </div>
+     <div className="book-div">
+        <div className="image-container">
+            <img className='image' alt="" src='https://github.com/MohamedAhmeDdev/eBook-Next-/blob/master/server/Images/1671463217357.jpg?raw=true' />
+        </div>
+        <div className="item-container">
+            <p>name</p>
+            <small>Ipsum duis nulla non reprehenderit labore incididunt.Ex amet sit minim et. </small>
+            <p>ksh 1000</p>
+        <div className='div-button'>
+        <button class="buy-button">Buy Now</button>
+        </div>
+        </div>
+    </div>
 
-      <div className="book-div">
-              <div className="image-container">
-                  <img className='image' alt="" src='https://raw.githubusercontent.com/MohamedAhmeDdev/eBook-Next-/master/server/Images/1671472657469.webp'  />
-              </div>
-              <div className="item-container">
-                  <p>name</p>
-                  <small>Ipsum duis nulla non reprehenderit labore incididunt.Ex amet sit minim et. </small>
-                  <p>ksh 1000</p>
-              </div>
-      </div>
 
-      <div className="book-div">
-              <div className="image-container">
-                  <img className='image' alt="" src='https://raw.githubusercontent.com/MohamedAhmeDdev/eBook-Next-/master/server/Images/1671476451167.webp'  />
-              </div>
-              <div className="item-container">
-                  <p>name</p>
-                  <small>Ipsum duis nulla non reprehenderit labore incididunt.Ex amet sit minim et. </small>
-                  <p>ksh 1000</p>
-              </div>
-      </div>
+    <div className="book-div">
+        <div className="image-container">
+            <img className='image' alt="" src='https://github.com/MohamedAhmeDdev/eBook-Next-/blob/master/server/Images/1671463217357.jpg?raw=true' />
+        </div>
+        <div className="item-container">
+            <p>name</p>
+            <small>Ipsum duis nulla non reprehenderit labore incididunt.Ex amet sit minim et. </small>
+            <p>ksh 1000</p>
+        <div className='div-button'>
+        <button class="buy-button">Buy Now</button>
+        </div>
+        </div>
+    </div>
+
+    <div className="book-div">
+        <div className="image-container">
+            <img className='image' alt="" src='https://github.com/MohamedAhmeDdev/eBook-Next-/blob/master/server/Images/1671463217357.jpg?raw=true' />
+        </div>
+        <div className="item-container">
+            <p>name</p>
+            <small>Ipsum duis nulla non reprehenderit labore incididunt.Ex amet sit minim et. </small>
+            <p>ksh 1000</p>
+        <div className='div-button'>
+        <button class="buy-button">Buy Now</button>
+        </div>
+        </div>
+    </div>
   </div>
 
   )
