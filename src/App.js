ab718ea4f5c85539796f488566961f0eb8e9cd60
Laurine Maina <mainalaureen58@gmail.com>
2024-05-08 20:38:44 +0300

    form post

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './Pages/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
 import BookShelves from './Pages/BookShelves';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
@@ -9,20 +9,48 @@ import FormData from './Pages/Form';
 import BookInfo from './Pages/BookInfo';
 
 function App() {
+  const [addBook, setAddBook] = useState({});
+
+  useEffect(() => {
+    if (Object.keys(addBook).length > 0) {
+      fetch("https://project2-db.onrender.com/books", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(addBook)
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('Failed to add book');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log("Book successfully added:", data);
+          
+          // Optionally, you can perform any additional actions after successful addition
+        })
+        .catch((error) => {
+          console.error("Error adding book:", error);
+        });
+    }
+  }, [addBook]);
+
   return (
     <div>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/MyShelves' element={<BookShelves/>} />
-        <Route path='/form' element={<FormData/>} />
-        <Route path='/bookinfo' element={<BookInfo/>} />
-      </Routes>
-      <Footer />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/MyShelves' element={<BookShelves />} />
+          <Route path='/form' element={<FormData setAddBook={setAddBook} />} />
+          <Route path='/bookinfo' element={<BookInfo />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -1,37 +1,64 @@
-import React from "react";
-import "./Form.css"
+import React, { useState } from "react";
+import "./Form.css";
 
-function FormData(){
-return(
+function FormData({ setAddBook }) {
+  const [formData, setFormData] = useState({
+    image: "",
+    title: "",
+    category: "",
+    description: "",
+    price: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  console.log("submitted",formData);
+
+    setAddBook(formData); // Pass the form data to the parent component
+    // Clear form fields after submission (optional)
+
+  };
+
+  return (
     <div>
-       
-    <form >
+      <form onSubmit={handleSubmit}>
         <p>Book Details</p>
 
         <label>Image:</label>
-        <input type="text" placeholder="Add Image Url" id="image" accept="image/*"/> <br/> <br/>
+        <input type="text" placeholder="Add Image Url" id="image" value={formData.image} onChange={handleChange} 
+        accept="image/*" />{formData.picture}  <br /> <br />
 
         <label>Title:</label>
-        <input type="text" placeholder="Title"/>  <br/> <br/>
+        <input type="text"placeholder="Title"id="title"value={formData.title}onChange={handleChange}/>{" "}
+         <br /> <br />
 
-       <label>Category:</label>
-        <input type="text" placeholder="Category"/> <br/> <br/>
+        <label>Category:</label>
+        <input type="text"placeholder="Category"id="category" value={formData.category}onChange={handleChange} />{" "}
+        <br /> <br />
 
         <label>Description:</label>
-        <input type="text" placeholder="Description"/> <br/> <br/>
+        <input type="text"placeholder="Description"id="description"value={formData.description} onChange={handleChange}/>{" "}
+        <br /> <br />
 
         <label>Price:</label>
-        <input type="text"placeholder="Price"/> <br/> <br/>
-      
+        <input type="text"placeholder="Price"id="price"value={formData.price} onChange={handleChange} />{" "}
+        <br /> <br />
 
-
-        <button id="btn" type="submit">Add a book</button> <br/> <br/>
+        {/* Change onSubmit to onClick */}
+        <button id="btn" type="submit"> Add a book </button> {" "}
+        <br /> <br />
 
         <footer id="footerform">Get to add a book of your choice.</footer>
-    </form>
-   
+      </form>
     </div>
-)
-
+  );
 }
-export default FormData
+
+export default FormData;
