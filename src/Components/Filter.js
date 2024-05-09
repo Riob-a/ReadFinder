bdfc0724fcdc1b29a440f89d1edfe8505558e95e
Laurine Maina <mainalaureen58@gmail.com>
2024-05-09 16:35:10 +0300

    filter_page

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,23 +1,25 @@
-import React from 'react';
-
-function Filter({ sortBooksByCategory} ) {
-           
-    const handleSort = (c) => {
-      sortBooksByCategory (c);
-    }
+function Filter({sortBooksByCategory}) {
 
 
   return (
-    <div className="container">
-      <p>Filter Book By Category</p>
-    <div className='button-container'> 
-      <button onClick={()=>handleSort('All')} >All</button>
-      <button onClick={()=>handleSort('Action')}>Action</button>
-      <button onClick={()=>handleSort('Fantasy')} >Fantasy</button>
-      <button onClick={()=>handleSort('Drama')}>Drama</button>
-    </div>
-   </div>
-  );
+    
+      <div className="container">
+        <p>Filter Books By Category</p>
+        <div className='button-container'>
+          <button onClick={() => sortBooksByCategory('All')}>All</button>
+          <button onClick={() => sortBooksByCategory('Action')}>Action</button>
+          <button onClick={() => sortBooksByCategory('Fantasy')}>Fantasy</button>
+          <button onClick={() => sortBooksByCategory('Drama')}>Drama</button>
+          <button onClick={() => sortBooksByCategory('Finance')}>Finance</button>
+          <button onClick={() => sortBooksByCategory('Horror')}>Horror</button>
+          <button onClick={() => sortBooksByCategory('Thriller')}>Thriller</button>
+          <button onClick={() => sortBooksByCategory('Theological')}>Theological</button>
+        </div>
+        </div>)
+    
+     
+     
+
 }
 
 export default Filter;
